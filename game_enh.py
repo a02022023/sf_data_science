f6c9f75c4e18e629f1211cc612a7e66e7a1bed1f
Anton <anton1707@gmail.com>
2023-02-02 17:05:25 +0100

    Updates in game_enh (code readability)

--- game_enh.py
+++ game_enh.py
@@ -1,22 +1,23 @@
-""" 26.01.2023 - Игра 'Угадай число', улучшенная версия """
+""" 02.02.2023 - Игра 'Угадай число', улучшенная версия с делением отрезка пополам"""
 import numpy as np
 left, right = 1, 100
-left_bracket, right_bracket = '[', ']'
+b_left, b_right = '[', ']'                      # b - сокращение от bracket
 number = np.random.randint(left, right+1)       # загадываем число
-count = 0                                       # количество попыток угадывания
+count = 0                                       # счетчик попыток угадывания
 
 while True:
     print('')
     count += 1
-    guessed_number = int(input(f"Попытка #{count}: Угадай число из диапазона {left_bracket}{left}; {right}{right_bracket}: "))
+    # Указываем номер текущей попытки и просим ввести угаданное число
+    guessed_number = int(input(f"Попытка #{count}: Угадай число из диапазона {b_left}{left}; {right}{b_right}: "))
     if number < guessed_number:
         print(f"Число должно быть меньше {guessed_number}")
         right = guessed_number
-        right_bracket = ')'
+        b_right = ')'
     elif number > guessed_number:
         print(f"Число должно быть больше {guessed_number}")
         left = guessed_number
-        left_bracket = '('
+        b_left = '('
     else:
         print(f"Ты угадал с {count}-й попытки, это число {number}!")
         break
